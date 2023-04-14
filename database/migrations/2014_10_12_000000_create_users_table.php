d5bf8799af6507caac675ffe94c72f1a1927d2db
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-14 18:06:03 +0000

    feat(database.users): add status
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status', false)->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
