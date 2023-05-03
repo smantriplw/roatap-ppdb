774e3547580ab9ddc83d34cd9043aa756dc39bfb
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-05-03 09:12:01 +0700

    feat: add tailwindcss
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PPDB SMAN 3 Palu - Maintenance</title>
+
+        <!-- CSS -->
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased">
+       <div class="flex h-screen">
+            <div class="m-auto text-center">
+                <h1 class="text-6xl font-bold text-red-500 uppercase">
+                    Maintenance
+                </h1>
+                <p class="text-center text-4xl">
+                    Situs PPDB sedang dalam maintenance, mohon coba beberapa saat lagi
+                </p>
+            </div>
+       </div>
+    </body>
+</html>
