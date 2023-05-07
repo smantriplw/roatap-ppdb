7d56e168eb09e6413ca68a5cceada719823d3857
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-05-07 02:15:35 +0000

    feat: setup cors origin
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Illuminate\Http\Middleware\HandleCors::class
     ];
 
     /**
@@ -42,7 +43,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\HandleCors::class
         ],
     ];
 
--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'config'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['ppdb.sman3palu.sch.id', 'certgensm.hanifs.tech'],
 
     'allowed_origins_patterns' => [],
 
