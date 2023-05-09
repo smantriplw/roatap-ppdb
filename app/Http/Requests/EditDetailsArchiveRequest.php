3e6e6fe9a9625a308d5603693468c6e057d625c5
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-05-09 03:25:22 +0000

    fix: fix unable to authorize
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- app/Http/Requests/EditDetailsArchiveRequest.php
+++ app/Http/Requests/EditDetailsArchiveRequest.php
@@ -14,7 +14,7 @@ class EditDetailsArchiveRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return !auth('archive')->check();
+        return auth('archive')->check();
     }
 
     /**
