f542dc1028c15eb012498327c9e18ee73cb5bbff
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-05-17 02:39:49 +0000

    feat: add zero points filter
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\Api\Users\ShowUserController;
 use App\Http\Middleware\JwtLogged;
 use App\Http\Middleware\OnlyActiveUser;
 use App\Http\Middleware\PesertaLogged;
+use App\Models\NilaiSemester;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -107,9 +108,16 @@ Route::group([
         PesertaLogged::class,
     ]);
     Route::get('/', function() {
+        $user = auth('archive')->user();
+        $zeroPoints = NilaiSemester::where('archive_id', $user->id)->where(function ($query) {
+            return $query->where('s1', 0)->orWhere('s2', 0)->orWhere('s3', 0)->orWhere('s4', 0)->orWhere('s5', 0);
+        });
+
         return response()->json([
             'error' => null,
-            'data' => auth('archive')->user(),
+            'data' => array_merge(json_decode(json_encode($user), true), [
+                'nilai_completed' => !$zeroPoints->exists(), 
+            ]),
             'message' => 'OK',
         ]);
     })->middleware([
