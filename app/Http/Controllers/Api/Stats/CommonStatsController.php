0171ad516b19e05534030a2d4e2a56165004d8b6
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-06-07 04:12:00 +0000

    feat: add todayVerified daily
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- app/Http/Controllers/Api/Stats/CommonStatsController.php
+++ app/Http/Controllers/Api/Stats/CommonStatsController.php
@@ -18,6 +18,8 @@ class CommonStatsController extends ApiController
                 'daily' => [
                     'todayCount' => $users->where('created_at', '>=', Carbon::today())->count(),
                     'lastWeek' => $users->where('created_at', '>=', Carbon::today()->subDays(7))->count(),
+
+                    'todayVerified' => $users->where('updated_at', '>=', Carbon::today())->where('verificator_id', '!=', null)->count(),
                 ],
                 'specified' => $users->countBy('type'),
             ],
