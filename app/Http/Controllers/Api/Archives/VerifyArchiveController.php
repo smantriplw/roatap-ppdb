da119d4d568f50513ff812ffd99645b521779f79
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-06-07 04:53:13 +0000

    feat: add unverify controller
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- app/Http/Controllers/Api/Archives/VerifyArchiveController.php
+++ app/Http/Controllers/Api/Archives/VerifyArchiveController.php
@@ -11,7 +11,7 @@ class VerifyArchiveController extends ApiController
     {
         $user = $request->user();
         $archive = Archive::find($id);
-        if (!isset($archive)) {
+        if (!isset($archive) || $archive->value('verificator_id') !== null) {
             return response()->json([
                 'error' => 'Archive doesn\'t exist',
             ], 400);
@@ -19,6 +19,24 @@ class VerifyArchiveController extends ApiController
 
         $archive->update([
             'verificator_id' => $user->id,
+        ]);
+
+        return response()->json([
+            'data' => 'Verified',
+        ]);
+    }
+
+    public function unverify(Request $request, string $id)
+    {
+        $archive = Archive::find($id);
+        if (!isset($archive) || $archive->value('verificator_id') == null) {
+            return response()->json([
+                'error' => 'Archive doesn\'t exist',
+            ], 400);
+        }
+
+        $archive->update([
+            'verificator_id' => null,
         ]);
 
         return response()->json([
