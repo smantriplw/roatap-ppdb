30197a9a28e2ca55fa9f40b51142f3ec36b9585e
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-06-10 09:38:30 +0000

    feat: filter prestasi, and afirmasi archive type to allow to ask the confirmation
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- app/Http/Controllers/Api/Archives/ConfirmArchiveController.php
+++ app/Http/Controllers/Api/Archives/ConfirmArchiveController.php
@@ -9,10 +9,17 @@ class ConfirmArchiveController extends Controller
 {
     public function confirm(ConfirmRequest $request)
     {
-        $confirmed = WawancaraConfirmModel::where('archive_id', auth('archive')->user()->id);
+        $user = auth('archive')->user();
+
+        if ($user->type !== 'afirmasi' || $user->type !== 'prestasi') {
+            return response()->json([
+                'error' => 'User status not eligible',
+            ], 400);
+        }
+        $confirmed = WawancaraConfirmModel::where('archive_id', $user->id);
         if (!$confirmed->exists()) {
             $confirmed = WawancaraConfirmModel::create(array_merge($request->all(), [
-                'archive_id' => auth('archive')->user()->id,
+                'archive_id' => $user->id,
             ]));
 
             return response()->json([
